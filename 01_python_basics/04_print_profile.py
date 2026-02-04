5623f5718e4c7b17a7aab98a2778a07897dd9ddb
Lakshan-Automations <2026lakshan@gmai.com>
2026-02-04 19:44:34 +0530

    add 04_print_profile

---
+++ 01_python_basics/04_print_profile.py
@@ -0,0 +1,12 @@
+print("=== Profile (with input) ===")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+country = input("Enter your country: ")
+goal = input("Enter your monthly income goal in USD: ")
+
+print("\n=== Summary ===")
+print(f"Name:{name}")
+print(f"Age:{age}")
+print(f"Country:{country}")
+print(f"Income Goal:{goal}USD/month")
