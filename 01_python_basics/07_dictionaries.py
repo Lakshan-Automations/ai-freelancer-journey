a2f484c698b80d0f027b6c928d64b33aa63e239a
Lakshan-Automations <2026lakshan@gmai.com>
2026-02-06 21:24:00 +0530

    python dictionaries

---
+++ 01_python_basics/07_dictionaries.py
@@ -0,0 +1,36 @@
+contact = {
+    "name":"lakshan",
+    "age":25,
+    "city":"Colombo"
+}
+
+contact["goal"] = "7k/month"
+del contact["city"]
+
+print(contact)
+
+print("name" in contact)
+
+student = {
+    "name": "John",
+    "scores": [85, 90, 78]
+}
+
+print(student["scores"])
+
+
+###task
+
+my_self = {
+    "name":"Lakshan",
+    "age":"25",
+    "favorite food":"papaya",
+    "city":"colombo"
+}
+
+my_self["goal"]= "Become $10k/month freelancer"
+my_self["city"]= "Melbourne"
+
+print(my_self["name"])
+print(my_self["goal"])
+
